30130fb710beb8fa8ce9f12d86118e6a609568a9
APEREZ2002 <119208468+APEREZ2002@users.noreply.github.com>
2024-03-12 20:07:41 +0100

    Ajuste imagen-tarjeta
    Ajuste imagen-tarjeta

--- src/app/inventario/inventario.component.ts
+++ src/app/inventario/inventario.component.ts
@@ -8,19 +8,18 @@ import { Component } from '@angular/core';
 export class InventarioComponent {
   // Asegúrate de que las rutas a las imágenes coincidan con donde las has guardado en tu proyecto
   objetos = [
-    { id: 1, nombre: 'Lollipop', imagen: 'assets/xuxes/xuxe1.png', descripcion: 'xuxe1', cantidad: 100  },
-    { id: 2, nombre: 'Cotton Candy', imagen: 'assets/xuxes/xuxe2.png', descripcion: 'xuxe2', cantidad: 10  },
-    { id: 3, nombre: 'Chocolate Bar', imagen: 'assets/xuxes/xuxe3.png', descripcion: 'xuxe3', cantidad: 20  },
-    { id: 4, nombre: 'Candy', imagen: 'assets/xuxes/xuxe4.png', descripcion: 'xuxe4', cantidad: 30  },
-    { id: 5, nombre: 'Candy Cane', imagen: 'assets/xuxes/xuxe5.png', descripcion: 'xuxe5', cantidad: 40  },
-    { id: 6, nombre: 'More Candy', imagen: 'assets/xuxes/xuxe6.png', descripcion: 'xuxe6' , cantidad: 50 },
-    { id: 7, nombre: 'Even More Candy', imagen: 'assets/xuxes/xuxe7.png', descripcion: 'xuxe7', cantidad: 60  },
-    { id: 8, nombre: 'Assorted Candies', imagen: 'assets/xuxes/xuxe8.png', descripcion: 'xuxe8' , cantidad: 70 },
-    { id: 9, nombre: 'Even More Candy', imagen: 'assets/xuxes/xuxe9.png', descripcion: 'xuxe9', cantidad: 80  },
-    { id: 10, nombre: 'Assorted Candies', imagen: 'assets/xuxes/xuxe10.png', descripcion: 'xuxe10' , cantidad: 90 }
-
-
-  ];
+    { id: 11, nombre: 'Monedas', imagen: 'assets/coins.png', cantidad: 200 },
+    { id: 1, nombre: 'Caramelos', imagen: 'assets/xuxes/xuxe1.png', cantidad: 100 },
+    { id: 2, nombre: 'Piruleta ', imagen: 'assets/xuxes/xuxe2.png', cantidad: 10 },
+    { id: 3, nombre: 'Piruleta Lazo', imagen: 'assets/xuxes/xuxe3.png', cantidad: 20 },
+    { id: 4, nombre: 'Algodon de Azucar', imagen: 'assets/xuxes/xuxe4.png', cantidad: 30 },
+    { id: 5, nombre: 'Tableta de Chocolate', imagen: 'assets/xuxes/xuxe5.png', cantidad: 40 },
+    { id: 6, nombre: 'Caramelo', imagen: 'assets/xuxes/xuxe6.png', cantidad: 50 },
+    { id: 7, nombre: 'Baston de Caramelo', imagen: 'assets/xuxes/xuxe7.png', cantidad: 60 },
+    { id: 8, nombre: 'Caramelo Largo', imagen: 'assets/xuxes/xuxe8.png', cantidad: 70 },
+    { id: 9, nombre: 'Carmelo Redondo y Largo', imagen: 'assets/xuxes/xuxe9.png', cantidad: 80 },
+    { id: 10, nombre: 'Surtido Caramelos', imagen: 'assets/xuxes/xuxe10.png', cantidad: 90 },
+];
 
   incrementarCantidadAleatoria() {
     // Selecciona un objeto al azar.
